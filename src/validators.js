7b39291d087546b770e724521609b94943dd67fa
Mike Cojocari <mihai040493@gmail.com>
2020-12-09 23:36:04 +0200

    added option keys validator

--- src/validators.js
+++ src/validators.js
@@ -1,5 +1,10 @@
 export function validateOptions(customOptions, defaultOptions) {
-	const instanceOptions = { ...defaultOptions, ...customOptions };
+	const allParametersInclouded = reqParams.every((item) =>
+		defaultOptions.some((param) => param.name === item)
+	);
+	if (!allParametersInclouded) throw new Error('Unrecognized option!');
+
+	return { ...defaultOptions, ...customOptions };
 }
 export function dateFormatValidator(format) {
 	const validator = /^(?:(d{1,4}|m{1,4}|y{4}|y{2})?\b(?:(?:,\s)|[-\s\/]{1})?(d{1,4}|m{1,4}|y{4}|y{2})?\b(?:(?:,\s)|[-\s\/]{1})?(d{1,4}|m{1,4}|y{4}|y{2})\b(?:(?:,\s)|[-\s\/]{1})?(d{1,4}|m{1,4}|y{2}|y{4})?\b)$/gi;
@@ -20,3 +25,41 @@ export function dateFormatValidator(format) {
 }
 
 export function eventValidator(event, eventDefaults) {}
+
+const isArray = (variable) => {
+	return Array.isArray(variable);
+};
+
+const isDate = (variable) => {
+	return variable instanceof Date;
+};
+
+const isString = (variable) => {
+	return typeof variable === 'string' || variable instanceof String ? true : false;
+};
+
+const Is = (variable) => {
+	const type = Object.prototype.toString
+		.call(variable)
+		.match(/\s([a-zA-Z]+)/)[1]
+		.toLowerCase();
+	const object = () => {
+		return type === 'object' ? true : false;
+	};
+	const array = () => {
+		return type === 'array' ? true : false;
+	};
+	const date = () => {
+		return type === 'date' ? true : false;
+	};
+	const number = () => {
+		return type === 'number' ? true : false;
+	};
+	const string = () => {
+		return type === 'string' ? true : false;
+	};
+	const boolean = () => {
+		return type === 'boolean' ? true : false;
+	};
+	return { object, array, date, number, string, boolean };
+};
