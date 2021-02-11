dca90951f1245fa35e2357181fb21f4784cae366
Mike <mihai040493@gmail.com>
2021-02-11 11:42:06 +0200

    cleaned template

--- src/js/template.js
+++ src/js/template.js
@@ -65,58 +65,58 @@ export default `<div class="mc-display">
 </thead>
 <tbody class="mc-table__body">
 <tr class="mc-table__week">
-<td class="mc-date mc-date--inactive" data-val-date="Sun Dec 28 1969 02:00:00 GMT+0200 (Eastern European Standard Time)">28</td>
-<td class="mc-date mc-date--inactive" data-val-date="Mon Dec 29 1969 02:00:00 GMT+0200 (Eastern European Standard Time)">29</td>
-<td class="mc-date mc-date--inactive" data-val-date="Tue Dec 30 1969 02:00:00 GMT+0200 (Eastern European Standard Time)">30</td>
-<td class="mc-date mc-date--inactive" data-val-date="Wed Dec 31 1969 02:00:00 GMT+0200 (Eastern European Standard Time)">31</td>
-<td class="mc-date mc-date--active" data-val-date="Thu Jan 01 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">1</td>
-<td class="mc-date mc-date--active" data-val-date="Fri Jan 02 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">2</td>
-<td class="mc-date mc-date--active" data-val-date="Sat Jan 03 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">3</td>
+<td class="mc-date mc-date--inactive" data-val-date="">28</td>
+<td class="mc-date mc-date--inactive" data-val-date="">29</td>
+<td class="mc-date mc-date--inactive" data-val-date="">30</td>
+<td class="mc-date mc-date--inactive" data-val-date="">31</td>
+<td class="mc-date mc-date--active" data-val-date="">1</td>
+<td class="mc-date mc-date--active" data-val-date="">2</td>
+<td class="mc-date mc-date--active" data-val-date="">3</td>
 </tr>
 <tr class="mc-table__week">
-<td class="mc-date mc-date--active" data-val-date="Sun Jan 04 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">4</td>
-<td class="mc-date mc-date--active" data-val-date="Mon Jan 05 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">5</td>
-<td class="mc-date mc-date--active" data-val-date="Tue Jan 06 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">6</td>
-<td class="mc-date mc-date--active" data-val-date="Wed Jan 07 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">7</td>
-<td class="mc-date mc-date--active" data-val-date="Thu Jan 08 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">8</td>
-<td class="mc-date mc-date--active" data-val-date="Fri Jan 09 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">9</td>
-<td class="mc-date mc-date--active" data-val-date="Sat Jan 10 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">10</td>
+<td class="mc-date mc-date--active" data-val-date="">4</td>
+<td class="mc-date mc-date--active" data-val-date="">5</td>
+<td class="mc-date mc-date--active" data-val-date="">6</td>
+<td class="mc-date mc-date--active" data-val-date="">7</td>
+<td class="mc-date mc-date--active" data-val-date="">8</td>
+<td class="mc-date mc-date--active" data-val-date="">9</td>
+<td class="mc-date mc-date--active" data-val-date="">10</td>
 </tr>
 <tr class="mc-table__week">
-<td class="mc-date mc-date--active" data-val-date="Sun Jan 11 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">11</td>
-<td class="mc-date mc-date--active" data-val-date="Mon Jan 12 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">12</td>
-<td class="mc-date mc-date--active" data-val-date="Tue Jan 13 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">13</td>
-<td class="mc-date mc-date--active" data-val-date="Wed Jan 14 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">14</td>
-<td class="mc-date mc-date--active" data-val-date="Thu Jan 15 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">15</td>
-<td class="mc-date mc-date--active" data-val-date="Fri Jan 16 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">16</td>
-<td class="mc-date mc-date--active" data-val-date="Sat Jan 17 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">17</td>
+<td class="mc-date mc-date--active" data-val-date="">11</td>
+<td class="mc-date mc-date--active" data-val-date="">12</td>
+<td class="mc-date mc-date--active" data-val-date="">13</td>
+<td class="mc-date mc-date--active" data-val-date="">14</td>
+<td class="mc-date mc-date--active" data-val-date="">15</td>
+<td class="mc-date mc-date--active" data-val-date="">16</td>
+<td class="mc-date mc-date--active" data-val-date="">17</td>
 </tr>
 <tr class="mc-table__week">
-<td class="mc-date mc-date--active" data-val-date="Sun Jan 18 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">18</td>
-<td class="mc-date mc-date--active" data-val-date="Mon Jan 19 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">19</td>
-<td class="mc-date mc-date--active" data-val-date="Tue Jan 20 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">20</td>
-<td class="mc-date mc-date--active" data-val-date="Wed Jan 21 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">21</td>
-<td class="mc-date mc-date--active" data-val-date="Thu Jan 22 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">22</td>
-<td class="mc-date mc-date--active" data-val-date="Fri Jan 23 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">23</td>
-<td class="mc-date mc-date--active" data-val-date="Sat Jan 24 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">24</td>
+<td class="mc-date mc-date--active" data-val-date="">18</td>
+<td class="mc-date mc-date--active" data-val-date="">19</td>
+<td class="mc-date mc-date--active" data-val-date="">20</td>
+<td class="mc-date mc-date--active" data-val-date="">21</td>
+<td class="mc-date mc-date--active" data-val-date="">22</td>
+<td class="mc-date mc-date--active" data-val-date="">23</td>
+<td class="mc-date mc-date--active" data-val-date="">24</td>
 </tr>
 <tr class="mc-table__week">
-<td class="mc-date mc-date--active" data-val-date="Sun Jan 25 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">25</td>
-<td class="mc-date mc-date--active" data-val-date="Mon Jan 26 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">26</td>
-<td class="mc-date mc-date--active" data-val-date="Tue Jan 27 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">27</td>
-<td class="mc-date mc-date--active" data-val-date="Wed Jan 28 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">28</td>
-<td class="mc-date mc-date--active" data-val-date="Thu Jan 29 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">29</td>
-<td class="mc-date mc-date--active" data-val-date="Fri Jan 30 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">30</td>
-<td class="mc-date mc-date--active" data-val-date="Sat Jan 31 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">31</td>
+<td class="mc-date mc-date--active" data-val-date="">25</td>
+<td class="mc-date mc-date--active" data-val-date="">26</td>
+<td class="mc-date mc-date--active" data-val-date="">27</td>
+<td class="mc-date mc-date--active" data-val-date="">28</td>
+<td class="mc-date mc-date--active" data-val-date="">29</td>
+<td class="mc-date mc-date--active" data-val-date="">30</td>
+<td class="mc-date mc-date--active" data-val-date="">31</td>
 </tr>
 <tr class="mc-table__week">
-<td class="mc-date mc-date--inactive" data-val-date="Sun Feb 01 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">1</td>
-<td class="mc-date mc-date--inactive" data-val-date="Mon Feb 02 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">2</td>
-<td class="mc-date mc-date--inactive" data-val-date="Tue Feb 03 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">3</td>
-<td class="mc-date mc-date--inactive" data-val-date="Wed Feb 04 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">4</td>
-<td class="mc-date mc-date--inactive" data-val-date="Thu Feb 05 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">5</td>
-<td class="mc-date mc-date--inactive" data-val-date="Fri Feb 06 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">6</td>
-<td class="mc-date mc-date--inactive" data-val-date="Sat Feb 07 1970 02:00:00 GMT+0200 (Eastern European Standard Time)">7</td>
+<td class="mc-date mc-date--inactive" data-val-date="">1</td>
+<td class="mc-date mc-date--inactive" data-val-date="">2</td>
+<td class="mc-date mc-date--inactive" data-val-date="">3</td>
+<td class="mc-date mc-date--inactive" data-val-date="">4</td>
+<td class="mc-date mc-date--inactive" data-val-date="">5</td>
+<td class="mc-date mc-date--inactive" data-val-date="">6</td>
+<td class="mc-date mc-date--inactive" data-val-date="">7</td>
 </tr>
 </tbody>
 </table>
