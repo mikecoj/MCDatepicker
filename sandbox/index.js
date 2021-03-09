a0ff346ef9b9d3b03d735b3860f931c15ae6bbdc
Mike <mihai040493@gmail.com>
2021-03-09 09:26:50 +0200

    merged and resolved the conflict

--- sandbox/index.js
+++ sandbox/index.js
@@ -8,7 +8,11 @@ const firstDatePicker = MCDatepicker.create({
 	bodyType: 'inline',
 	dateFormat: 'dddd, dd mmmm.yyyy',
 	disableDates: [new Date(2021, 1, 12), new Date(2021, 3, 15)],
+<<<<<<< HEAD
 	customWeekDays: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+=======
+	customWeekDays: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+>>>>>>> 0a189c89af9581f593936f639c6f959f5b47ba4d
 	firstWeekday: 1
 });
 
