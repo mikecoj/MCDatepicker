6c4aa6d50dbf667c4e0c61645ec374f47d87f82a
mikecoj <mihai040493@gmail.com>
2021-04-04 22:52:32 +0300

    added calndarNodes to mediator

--- src/js/mc-calendar.js
+++ src/js/mc-calendar.js
@@ -1,6 +1,6 @@
 import { noop } from './utils';
 import defaultOptions from './defaults';
-import { applyOnFocusListener, removeOnFocusListener } from './handlers';
+import { applyOnFocusListener, removeOnFocusListener } from './listeners';
 import createInstance from './instance';
 import { validateOptions } from './validators';
 import { writeTemplate } from './render';
@@ -10,24 +10,27 @@ import '../scss/main.scss';
 
 const MCDatepicker = (() => {
 	let datepickers = [];
-	let calendarDiv = null;
+	let calendarNodes = null;
 
 	let initCalendar = () => {
 		initCalendar = noop;
-		calendarDiv = writeTemplate(datepickers);
+		calendarNodes = writeTemplate(datepickers);
 	};
 
 	const open = (el) => {
-		dispatchCalendarShow(calendarDiv, el);
+		const { calendar } = calendarNodes;
+		dispatchCalendarShow(calendar, el);
 	};
 
 	const close = () => {
-		dispatchCalendarHide(calendarDiv);
+		const { calendar } = calendarNodes;
+		dispatchCalendarHide(calendar);
 	};
 
 	const create = (options = {}) => {
 		// initiate the calendar instance once
 		initCalendar();
+		const { calendar } = calendarNodes;
 		// validate options and merge them with de default Options
 		const instanceOptions = validateOptions(options, defaultOptions);
 		// create instance
@@ -36,7 +39,7 @@ const MCDatepicker = (() => {
 		datepickers.push(instance);
 		// add event listener to the linked input
 		if (options.hasOwnProperty('el')) {
-			applyOnFocusListener(calendarDiv, instance);
+			applyOnFocusListener(calendar, instance);
 		}
 
 		return instance;
