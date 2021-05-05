4564e5d8e341adde59bc0f61552037bf1e4b43a4
Mike <mihai040493@gmail.com>
2021-05-05 13:55:11 +0300

    fixed calendar re-rendering

--- src/js/mc-calendar.js
+++ src/js/mc-calendar.js
@@ -1,10 +1,9 @@
-import { noop } from './utils';
 import defaultOptions from './defaults';
-import { applyOnFocusListener, removeOnFocusListener } from './listeners';
 import createInstance from './instance';
-import { validateOptions } from './validators';
 import { writeTemplate } from './render';
+import { validateOptions } from './validators';
 import { dispatchCalendarShow, dispatchCalendarHide } from './emiters';
+import { applyOnFocusListener, removeOnFocusListener } from './listeners';
 
 import '../scss/main.scss';
 
@@ -12,14 +11,14 @@ const MCDatepicker = (() => {
 	let datepickers = [];
 	let calendarNodes = null;
 
-	let initCalendar = () => {
-		initCalendar = noop;
+	const initCalendar = () => {
+		if (calendarNodes) return;
 		calendarNodes = writeTemplate(datepickers);
 	};
 
-	const open = (el) => {
+	const open = (instance) => {
 		const { calendar } = calendarNodes;
-		dispatchCalendarShow(calendar, el);
+		dispatchCalendarShow(calendar, instance);
 	};
 
 	const close = () => {
@@ -30,7 +29,6 @@ const MCDatepicker = (() => {
 	const create = (options = {}) => {
 		// initiate the calendar instance once
 		initCalendar();
-		const { calendar } = calendarNodes;
 		// validate options and merge them with de default Options
 		const instanceOptions = validateOptions(options, defaultOptions);
 		// create instance
@@ -38,19 +36,22 @@ const MCDatepicker = (() => {
 		// push fresh created instance to instances array
 		datepickers.push(instance);
 		// add event listener to the linked input
-		if (options.hasOwnProperty('el')) {
-			applyOnFocusListener(calendar, instance);
-		}
+		applyOnFocusListener(calendarNodes.calendar, instance);
 
 		return instance;
 	};
 	const remove = (instance) => {
+		if (!datepickers.length) return;
 		// remove the onFocus listener
-		if (options.hasOwnProperty('el')) {
-			removeOnFocusListener(instance);
-		}
+		removeOnFocusListener(instance);
 		// Remove the instance from the datepickers array
 		datepickers.splice(datepickers.indexOf(instance), 1);
+
+		if (!datepickers.length) {
+			const { calendar } = calendarNodes;
+			calendar.parentNode.removeChild(calendar);
+			calendarNodes = null;
+		}
 	};
 	return { create, remove, open, close };
 })();
