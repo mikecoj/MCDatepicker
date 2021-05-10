57686a2ea336ea3dee98e21f34c8753780183178
Mike <mihai040493@gmail.com>
2021-05-10 12:17:46 +0300

    added dblclick prop to PREVIEW_PICK & DATE_PICK custom event object

--- src/js/emiters.js
+++ src/js/emiters.js
@@ -43,11 +43,12 @@ export const dispatchHeaderUpdate = (elem) => {
 	elem.dispatchEvent(new CustomEvent(HEADER_UPDATE, { bubbles: true }));
 };
 
-export const dispatchDatePick = (elem) => {
+export const dispatchDatePick = (elem, dblclick = false) => {
 	elem.dispatchEvent(
 		new CustomEvent(DATE_PICK, {
 			bubbles: true,
 			detail: {
+				dblclick,
 				date: new Date(elem.getAttribute('data-val-date'))
 			}
 		})
@@ -72,13 +73,12 @@ export const dispatchChangeYear = (elem, direction) => {
 	);
 };
 
-export const dispatchPreviewCellPick = (elem) => {
-	const target = elem.offsetParent.getAttribute('data-target');
+export const dispatchPreviewCellPick = (elem, dblclick = false) => {
 	elem.dispatchEvent(
 		new CustomEvent(PREVIEW_PICK, {
 			bubbles: true,
 			detail: {
-				target,
+				dblclick,
 				data: elem.children[0].innerHTML
 			}
 		})
